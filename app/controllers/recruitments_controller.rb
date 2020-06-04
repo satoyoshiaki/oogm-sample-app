e567a868c88a849982d3bef19e737e284d956ac4
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-04 23:53:53 +0900

    recruitmentのコントローラー作成

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,12 +1,36 @@
 class RecruitmentsController < ApplicationController
-    def new
-        @recruitment = Recruitment.new
-    end
-
-    def create
-    end
-
+    before_action :set_recruitment, only: [:show, :edit, :update]
     def index
         @recruitment = Recruitment.all
-    end 
+      end
+      def new
+        @recruitment = Recruitment.new
+      end
+      def create
+        @recruitment = Recruitment.new(recruitment_params)
+        if @recruitment.save
+          redirect_to recruitment_path, notice: "ブログを作成しました！"
+        else
+          render :new
+        end
+      end
+      def show
+      end
+      def edit
+      end
+      def update
+        if @recruitment.update(recruitment_params)
+          redirect_to recruitments_path, notice: "ブログを編集しました！"
+        else
+          render :edit
+        end
+      end
+      private
+      def recruitment_params
+        params.require(:blog).permit(:title, :content)
+      end
+      # idをキーとして値を取得するメソッドを追加
+      def set_recruitment
+        @recruitment = Recruitment.find(params[:id])
+      end
 end
