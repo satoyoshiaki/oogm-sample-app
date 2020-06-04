ffa1c5fbe7afdc5a32adcbb1287d85e1f786b2a7
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-04 22:32:03 +0900

    recruitmentのバリデーション 追加

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,2 +1,9 @@
 class Recruitment < ApplicationRecord
+    validates :play_now, presence: true
+    validates :game_title, presence: true
+    validates :comment, presence: true
+    validates :machine, presence: true
+    validates :image, presence: true
+    validates :on_off, presence: true
+    validates :purpouse, presence: true
 end
