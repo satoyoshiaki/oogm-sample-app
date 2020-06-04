c6397b39e2511b101a48c245219007fda26cc358
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-04 18:00:20 +0900

    create_table :recruitments do |t|

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,7 +1,12 @@
 class RecruitmentsController < ApplicationController
     def new
+        @recruitment = Recruitment.new
     end
 
     def create
     end
+
+    def index
+        @recruitment = Recruitment.all
+    end 
 end
