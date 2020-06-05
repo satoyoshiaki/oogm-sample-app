2272d0f25fbe410ef4726860f24b3205b80ceb06
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-05 16:42:10 +0900

    プルダウンメニュの完成(バリデーション のnumericalityがエラーの原因だった)

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -8,9 +8,8 @@ class Recruitment < ApplicationRecord
     validates :purpouse, presence: true
     validates :comment,    length: { in: 1..250 }
     validates :game_title,    length: { in: 1..30 }
-    validates :play_now, numericality: true
 
-    enum play_now: { 今遊ぶ！: 0, 定期的に: 1, あとで: 2, いつか: 3 }
+    enum play_now: { 今遊ぶ: 0, 定期的に: 1, あとで: 2, いつか: 3 }
     enum machine: { PC: 0, スマホ: 1, swhich: 2, PS4: 3, PSVITA: 4, PS5: 5, カードゲーム: 6, xboxone: 7, その他: 8 }
     enum on_off: { オンライン: 0, オフライン: 1 }
     enum purpouse: { フレンド: 0, 協力プレイ: 1, 対戦: 2, 練習: 3, 経験値稼ぎ: 4, イベント周回: 5, イベント参加: 6, 大会: 7, 楽しくお話: 8, その他の目的: 9 }
