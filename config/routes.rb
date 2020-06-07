9465db89b7d5644cdc54c42194e2339791e98ae1
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-07 17:25:13 +0900

    rootの追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  devise_scope :user do
+    get 'profile_edit', to: 'users/registrations#profile_edit', as: 'profile_edit'
+    patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
+  end
+
   root "recruitments#index"
 end
